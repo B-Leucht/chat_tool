df7bbad2db21ce6917e019f832ab10eb066ab60e
Benjamin Karl Josua Leucht <leuchtb@cip.ifi.lmu.de>
2023-10-30 18:19:15 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 import socket
+# get a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to my port so that it will accept connections from everywhere
 s.bind(('', 54321))
 s.listen(1)
+#wait for incoming connection
 conn, addr = s.accept()
+
 while True:
     data = conn.recv(1024)
-    print(data)
+
+    print(data.decode('utf-8'))
+
+# TODO smoothly exit the program
