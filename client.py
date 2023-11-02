50d2f3d0545f0e79c6fc13c9b5da0682f29f3ede
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-02 12:45:00 +0100

    client-name now gets printed in front of  messages

--- client.py
+++ client.py
@@ -4,10 +4,11 @@ import sys
 name = input('Name: ')
 # get a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# connect the socket to the port and local host
+# connect the socket to the server
 s.connect(('localhost', 54321))
 # send name to server
-s.sendall((name+' ist mit dem Server verbunden').encode('utf-8'))
+s.sendall((name).encode('utf-8'))
+# iterate over stdin and send the encoded strings to server
 for line in sys.stdin:
     s.sendall(line.encode('utf-8'))
 print("End.")
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import socket
 # get a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to my port so that it will accept connections from everywhere
+# bind the socket to my port so that it will accept connections from everywhere
 s.bind(('', 54321))
 s.listen(1)
-#wait for incoming connection
+# wait for incoming connection
 conn, addr = s.accept()
+# get the clients name and decode it
+name = conn.recv(1024).decode('utf-8')
 
 while True:
     data = conn.recv(1024)
-    print(data.decode('utf-8'))
+    print(name+': '+data.decode('utf-8'))
