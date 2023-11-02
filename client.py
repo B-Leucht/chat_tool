31965d2be20a48e421ce25778837da9ae866372a
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-02 12:30:16 +0100

    added comments to client.py

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
 import sys
+# ask user for their name
 name = input('Name: ')
+# get a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect the socket to the port and local host
 s.connect(('localhost', 54321))
-s.sendall(name+' ist mit dem Server verbunden').encode('utf-8'))
+# send name to server
+s.sendall((name+' ist mit dem Server verbunden').encode('utf-8'))
 for line in sys.stdin:
     s.sendall(line.encode('utf-8'))
 print("End.")
