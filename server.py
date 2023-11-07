b2a416a7b227e4a2d4aff000562c1414db7645b5
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-07 19:38:34 +0100

    server now broadcasts messages to all clients

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
 import socket
 import threading
 
-clients = []
+c_sockets = []
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 def handle_client(c_socket):
     """handles a clients interaction with the server"""
     name = c_socket.recv(1024).decode("utf-8")
-    clients.append((name, c_socket))
+    c_sockets.append(c_socket)
     while True:
         data = c_socket.recv(1024).decode("utf-8")
-        print(name + ": " + data)
+        message = name + ": " + data
+        print(message)
+        for sock in c_sockets:
+            sock.sendall(message.encode("utf-8"))
 
 
 if __name__ == "__main__":
     # get a socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # bind the socket to my port so that it will accept connections from everywhere
     s.bind(("", 54321))
     s.listen(1)
