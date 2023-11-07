c3c8ddec45ad26c5a8e62ef252e917538033e2a2
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-07 19:14:21 +0100

    server can now handle multiple clients

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 import socket
-import sys
-# ask user for their name
-name = input('Name: ')
-# get a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# connect the socket to the server
-s.connect(('localhost', 54321))
-# send name to server
-s.sendall((name).encode('utf-8'))
-# iterate over stdin and send the encoded strings to server
-for line in sys.stdin:
-    s.sendall(line.encode('utf-8'))
-print("End.")
+
+if __name__ == "__main__":
+    # ask user for their name
+    name = input("Name: ")
+    # get a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect the socket to the server
+    s.connect(("localhost", 54321))
+    # send name to server
+    s.sendall(name.encode("utf-8"))
+    while True:
+        line = input("You: ")
+        s.sendall(line.encode("utf-8"))
--- server.py
+++ server.py
@@ -1,14 +1,26 @@
 import socket
-# get a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to my port so that it will accept connections from everywhere
-s.bind(('', 54321))
-s.listen(1)
-# wait for incoming connection
-conn, addr = s.accept()
-# get the clients name and decode it
-name = conn.recv(1024).decode('utf-8')
+import threading
 
-while True:
-    data = conn.recv(1024)
-    print(name+': '+data.decode('utf-8'))
+clients = []
+
+
+def handle_client(c_socket):
+    """handles a clients interaction with the server"""
+    name = c_socket.recv(1024).decode("utf-8")
+    clients.append((name, c_socket))
+    while True:
+        data = c_socket.recv(1024).decode("utf-8")
+        print(name + ": " + data)
+
+
+if __name__ == "__main__":
+    # get a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to my port so that it will accept connections from everywhere
+    s.bind(("", 54321))
+    s.listen(1)
+    while True:
+        # wait for incoming connection
+        c_socket, addr = s.accept()
+        c_thread = threading.Thread(target=handle_client, args=(c_socket,))
+        c_thread.start()
