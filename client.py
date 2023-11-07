0d3af8b5486647d9257348b915e9127dfcdd3eb3
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-07 19:39:18 +0100

    client now receives messages and prints them

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 import socket
+import sys
+import threading
+
+# get a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def receive():
+    '''Handles incoming messages'''
+    while True:
+        data = s.recv(1024).decode("utf-8")
+        print(data)
+
 
 if __name__ == "__main__":
     # ask user for their name
     name = input("Name: ")
-    # get a socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # connect the socket to the server
     s.connect(("localhost", 54321))
     # send name to server
     s.sendall(name.encode("utf-8"))
-    while True:
-        line = input("You: ")
+    r_thread = threading.Thread(target=receive)
+    r_thread.start()
+    for line in sys.stdin:
         s.sendall(line.encode("utf-8"))
