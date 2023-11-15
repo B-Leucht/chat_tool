bace2674436fefffd28327e0170c6f3d26f07cbf
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-15 14:34:37 +0100

    added exception handling for disconnected clients

--- server.py
+++ server.py
@@ -1,35 +1,46 @@
 import socket
 import threading
 
-c_sockets = []
+# a set of all
+client_sockets = set()
 # get a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 
 def handle_client(c_socket):
     """handles a clients interaction with the server"""
     # receive name from client
-    name = c_socket.recv(1024).decode("utf-8")
+    try:
+        name = c_socket.recv(1024).decode("utf-8")
+    except ConnectionResetError:
+        return
     # add socket to list of all sockets
-    c_sockets.append(c_socket)
+    client_sockets.add(c_socket)
     while True:
-        #receive data from client
-        data = c_socket.recv(1024).decode("utf-8")
-        #concatenate name with data
+        # receive data from client
+        try:
+            data = c_socket.recv(1024).decode("utf-8")
+        except ConnectionResetError:
+            return
+        # concatenate name with data
         message = name + ": " + data
         print(message)
-        #broadcast message to all clients
+        # duplicate the clients_sockets set, so it can be changed while iterating over it
+        c_sockets = set(client_sockets)
+        # broadcast message to all clients
         for sock in c_sockets:
-            sock.sendall(message.encode("utf-8"))
+            try:
+                sock.sendall(message.encode("utf-8"))
+            except BrokenPipeError:
+                client_sockets.remove(sock)
 
 
 if __name__ == "__main__":
-    # bind the socket to my port so that it will accept connections from everywhere
+    # bind the socket to my port
     s.bind(("", 54321))
-    s.listen(1)
+    s.listen(2)
     while True:
         # wait for incoming connection
         c_socket, addr = s.accept()
-        # create thread for new client 
+        # create thread for new client
         c_thread = threading.Thread(target=handle_client, args=(c_socket,))
         c_thread.start()
