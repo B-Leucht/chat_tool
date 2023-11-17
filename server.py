fc7b00b4e9774e5399c01bd3d35a947bf31c882c
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-17 17:00:43 +0100

    added connection and disconnection messages

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 import socket
 import threading
 
-# a set of all
+# a set of all client sockets
 client_sockets = set()
-# get a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def handle_client(c_socket):
     """handles a clients interaction with the server"""
@@ -15,30 +13,38 @@ def handle_client(c_socket):
         return
     # add socket to list of all sockets
     client_sockets.add(c_socket)
+    message = "User " + name + " connected"
+    broadcast(message)
     while True:
         # receive data from client
         try:
             data = c_socket.recv(1024).decode("utf-8")
         except ConnectionResetError:
-            print("User " + name + " disconnected")
+            message = "User " + name + " disconnected"
+            broadcast(message)
             break
-        if data != "": # make sure the message isn't an empty string
+        if data != "": # make sure the message isn't an empty stri
             # concatenate name with data
             message = name + ": " + data
-            print(message)
-            # duplicate the clients_sockets set, so it can be changed while iterating over it
-            c_sockets = set(client_sockets)
-            # broadcast message to all clients
-            for sock in c_sockets:
-                try:
-                    # send message to client
-                    sock.sendall(message.encode("utf-8"))
-                except BrokenPipeError:
-                    # remove socket from set if it can't be reached
-                    client_sockets.remove(sock)
+            broadcast(message)
 
+def broadcast(message):
+    print(message)
+    # duplicate the clients_sockets set, so it can be changed while iterating over it
+    c_sockets = set(client_sockets)
+    # broadcast message to all clients
+    for sock in c_sockets:
+        try:
+            # send message to client
+            sock.sendall(message.encode("utf-8"))
+        except BrokenPipeError:
+            # remove socket from set if it can't be reached
+            client_sockets.remove(sock)
+    
 
 if __name__ == "__main__":
+    # get a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
     # bind the socket to my port
     s.bind(("", 54321))
     s.listen(2)
