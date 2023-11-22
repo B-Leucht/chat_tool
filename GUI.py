adf234eff333301f5e1d97004cb0ef6e43116a6e
B-Leucht <124815189+B-Leucht@users.noreply.github.com>
2023-11-22 13:41:18 +0100

    wrap_text now handles single words thast exceed the max_width

--- GUI.py
+++ GUI.py
@@ -1,10 +1,12 @@
 import pygame
 import sys
 
+
 class GUI:
     """
     The GUI class represents the graphical user interface for a simple chat tool.
     """
+
     def __init__(self, client=None):
         """
         Initialize the GUI.
@@ -34,26 +36,26 @@ class GUI:
         self.active = False
         self.text = ""
         self.text_surface = self.font.render(self.text, True, self.color)
-        self.width = max(200, self.text_surface.get_width() + 10)
+        self.total_chat_height = 0
         self.scroll_offset = 0
         self.chat_area = pygame.Rect(50, 50, self.WIDTH - 100, self.HEIGHT - 150)
         self.chat_log = []
 
         # Initialize clock for controlling the frame rate
         self.clock = pygame.time.Clock()
-
-        # Name input popup properties
-        self.popup_active = True
-        self.popup_text = ""
-        self.popup_input_box = pygame.Rect(self.WIDTH // 4, self.HEIGHT // 4, self.WIDTH // 2, 32)
-        self.name = self.get_user_name()
+        
 
     def show_name_popup(self):
         """
         Display a pop-up window to get the user's name.
         """
+        popup_active = True
+        popup_text = ""
+        popup_input_box = pygame.Rect(
+            self.WIDTH // 4, self.HEIGHT // 4, self.WIDTH // 2, 32
+        )
         # Continue the loop while the popup window is active
-        while self.popup_active:
+        while popup_active:
             # Iterate through all Pygame events
             for event in pygame.event.get():
                 # Check if the event is a window close event
@@ -67,38 +69,41 @@ class GUI:
                     # Check if the Enter/Return key is pressed
                     if event.key == pygame.K_RETURN:
                         # Disable the popup window
-                        self.popup_active = False
+                        popup_active = False
                     # Check if the Backspace key is pressed
                     elif event.key == pygame.K_BACKSPACE:
                         # Remove the last character from the entered text
-                        self.popup_text = self.popup_text[:-1]
+                        popup_text = popup_text[:-1]
                     else:
                         # Add the typed character to the entered text
-                        self.popup_text += event.unicode
-
+                        if self.font.size(popup_text)[0] < popup_input_box.width-20:
+                            popup_text += event.unicode
             # Fill the screen with the background color
             self.screen.fill(self.WHITE)
 
             # Draw the pop-up window border
-            pygame.draw.rect(self.screen, self.color_active, self.popup_input_box, 2)
+            pygame.draw.rect(self.screen, self.color_active, popup_input_box, 2)
 
             # Render the "Username:" text on the pop-up window
             username_text = self.font.render("Username:", True, self.BLACK)
-            self.screen.blit(username_text, (self.popup_input_box.x + 5, self.popup_input_box.y - 30))
+            self.screen.blit(
+                username_text, (popup_input_box.x + 5, popup_input_box.y - 30)
+            )
 
             # Render the entered text on the pop-up window
-            popup_text_surface = self.font.render(self.popup_text, True, self.BLACK)
-            self.screen.blit(
-                popup_text_surface,
-                (self.popup_input_box.x + 5, self.popup_input_box.y + 5)
-            )
+            if popup_text:
+                popup_text_surface = self.font.render(popup_text, True, self.BLACK)
+                self.screen.blit(
+                    popup_text_surface,
+                 (popup_input_box.x + 5, popup_input_box.y + 5),
+                )
 
             # Update the display
             pygame.display.flip()
 
             # Control the frame rate
             self.clock.tick(30)
-
+        return popup_text
 
     def get_user_name(self):
         """
@@ -107,8 +112,7 @@ class GUI:
         Returns:
         - str: The user's name.
         """
-        self.show_name_popup()
-        return self.popup_text
+        return self.show_name_popup()
 
     def add_message(self, message):
         """
@@ -118,6 +122,11 @@ class GUI:
         - message (str): The message to be added.
         """
         self.chat_log.append(message)
+        # Calculate the new total height of the chat log
+        self.total_chat_height = int(sum(
+            ((len(message.splitlines()) * (self.FONT_SIZE + 5)) + 10)
+            for message in self.chat_log
+        ))
 
     def handle_events(self):
         """
@@ -145,17 +154,19 @@ class GUI:
 
                 # Check if the Up arrow key is pressed
                 if event.key == pygame.K_UP:
-                    # Scroll up if the chat log exceeds the visible chat area
-                    if len(self.chat_log) * (self.FONT_SIZE + 5) > self.chat_area.height - 20:
-                        if self.scroll_offset < (len(self.chat_log) - 1) * (self.FONT_SIZE + 5):
-                            self.scroll_offset += 20
+                    # Scroll up if the total height exceeds the visible chat area
+                    if (
+                        self.total_chat_height > self.chat_area.height + 5
+                        and self.total_chat_height - self.scroll_offset
+                        > self.chat_area.height
+                    ):
+                        self.scroll_offset += 20
 
                 # Check if the Down arrow key is pressed
                 if event.key == pygame.K_DOWN:
                     # Scroll down if the scroll offset is greater than zero
                     if self.scroll_offset > 0:
                         self.scroll_offset -= 20
-
 
     def handle_mouse_click(self, event):
         """
@@ -191,7 +202,7 @@ class GUI:
         """
         if self.text != "":
             if self.client is None:
-                self.chat_log.append(self.text)
+                self.add_message(self.text)
             else:
                 self.client.send_text(self.text)
             self.text = ""
@@ -218,10 +229,14 @@ class GUI:
         """
         Update the text surface used for rendering the input box text.
         """
-        lines = self.wrap_text(self.text, self.chat_area.width - 10)
-        last_line = lines[-1]
-        self.width = max(200, self.font.size(last_line)[0] + 10)
-        self.text_surface = self.font.render(last_line, True, self.color)
+
+        # Check if there are lines in the text
+        if self.text:
+            self.text = self.wrap_text(self.text, self.chat_area.width - 20)
+            last_line = self.text.splitlines()[-1]
+            self.text_surface = self.font.render(last_line, True, self.color)
+        else:
+            self.text_surface = self.font.render("", True, self.color)
 
     def draw_ui(self):
         """
@@ -231,8 +246,9 @@ class GUI:
         pygame.draw.rect(self.screen, self.BLACK, self.chat_area)
         self.display_chat_log()
         pygame.draw.rect(self.screen, self.color, self.input_box, 2)
-        self.screen.blit(self.text_surface, (self.input_box.x + 5, self.input_box.y + 5))
-        self.input_box.w = self.width
+        self.screen.blit(
+            self.text_surface, (self.input_box.x + 5, self.input_box.y + 5)
+        )
 
     def display_chat_log(self):
         """
@@ -243,8 +259,7 @@ class GUI:
 
         # Iterate through each line in the reversed chat log
         for line in reversed(self.chat_log):
-        # Wrap the text to fit within the specified width
-            lines = self.wrap_text(line, self.chat_area.width - 10)
+            lines = line.splitlines()
 
             # Iterate through each wrapped line in reversed order
             for line_text in reversed(lines):
@@ -255,56 +270,53 @@ class GUI:
                 if 0 <= y_offset <= self.chat_area.height - 20:
                     # Display the line of text on the screen at the calculated position
                     self.screen.blit(
-                        line_surface, (self.chat_area.x + 5, self.chat_area.y + y_offset)
+                        line_surface,
+                        (self.chat_area.x + 5, self.chat_area.y + y_offset),
                     )
 
                 # Adjust the y offset for the next line
                 y_offset -= self.FONT_SIZE + 5
-            
-    def wrap_text(self, text, max_width):
-        """
-        Wrap the text to fit within the specified width.
 
-        Parameters:
-        - text (str): The text to wrap.
-        - max_width (int): The maximum width for the wrapped text.
+            y_offset -= 10
+
+    def wrap_text(self, text, max_width):
+        """wrap text into a certain max_width
+
+        Args:
+            text (str): text to be wrapped
+            max_width (int): width it should be wrapped to
 
         Returns:
-        - list: A list of wrapped lines.
+            str: wrapped text
         """
-        # Split the input text into a list of words
-        words = text.split(' ')
+        lines = []
+        for paragraph in text.split("\n"):
+            words = paragraph.split(" ")
+            current_line = []
 
-        # Initialize variables for wrapped lines and the current line
-        wrapped_lines = []
-        current_line = words[0]
+            for word in words:
+                # Check if adding the current word exceeds the maximum width
+                if self.font.size(" ".join(current_line + [word]))[0] <= max_width:
+                    current_line.append(word)
+                else:
+                    # Check if the current word itself is longer than the maximum width
+                    if self.font.size(word)[0] > max_width:
+                        # Split the long word into segments that fit the maximum width
+                        i=0
+                        while self.font.size(word[:i])[0]<max_width:
+                            i += 1
+                        lines.append(word[:i])
+                        if i < len(word):
+                            current_line = [word[i:]]
+                        else:
+                            current_line = []
+                    else:
+                        lines.append(" ".join(current_line))
+                        current_line = [word]
 
-        # Iterate through each word in the text
-        for word in words[1:]:
-            # Concatenate the current line with the next word
-            test_line = current_line + ' ' + word
-
-            # Get the width and height of the test line using the Pygame font
-            width, _ = self.font.size(test_line)
-            # If the current line itself exceeds the max_width, add it to wrapped_lines
-            # Check if the test line fits within the specified width
-            if width <= max_width:
-                # Update the current line with the test line if it fits
-                    current_line = test_line
-            else:
-                # Add the current line to the list of wrapped lines
-                wrapped_lines.append(current_line)
-
-                # Reset the current line to the current word
-                current_line = word
-
-        # Add the last remaining line to the list of wrapped lines
-        wrapped_lines.append(current_line)
-
-        # Return the list of wrapped lines
-        return wrapped_lines
-
-
+            if current_line:
+                lines.append(" ".join(current_line))
+        return "\n".join(lines)
 
     def run(self):
         """
@@ -319,4 +331,5 @@ class GUI:
 
 if __name__ == "__main__":
     gui = GUI()
+    gui.get_user_name()
     gui.run()
